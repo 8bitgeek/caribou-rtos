2c47d2c94fffcb4e153a06a120d44fcbefd1464c
8bitgeek <mike@8bitgeek.net>
2016-03-12 14:54:07 -0500

    CFile.h

--- caribou++/include/caribou++/cfile.h
+++ caribou++/include/caribou++/cfile.h
@@ -62,6 +62,7 @@ namespace CARIBOU
 			static FATFS*					fs()	{return mFileSystem;}
 
 			static CARIBOU::CString			md5(CARIBOU::CString filePath);
+			static CARIBOU::CString			tempFile(CARIBOU::CString dir);
 
 		protected:
         	void							failureNotify(uint8_t err);
