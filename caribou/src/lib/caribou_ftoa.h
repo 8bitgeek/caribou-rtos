8bf2f859fa71e2fe01cbfeeb186fee25d3e14fa5
8bitgeek <mike@8bitgeek.net>
2018-08-10 13:30:18 -0400

    caribou ftoa added to lib
    %f support in stdio.c printf

---
+++ caribou/src/lib/caribou_ftoa.h
@@ -0,0 +1,14 @@
+#ifndef __CARIBOU_FTOA_H__
+#define __CARIBOU_FTOA_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+char * caribou_ftoa(float f, char * buf, int precision);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // __CARIBOU_FTOA_H__
