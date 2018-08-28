90aa876f6e94bb4e93e1fe799d2e0ee947efc13d
8bitgeek <mike@8bitgeek.net>
2018-08-28 12:00:53 -0400

    Cortex-M headers

--- caribou/target/cpu/arm/cortex-m0/cpu/cpu.h
+++ caribou/target/cpu/arm/cortex-m0/cpu/cpu.h
@@ -1,19 +1,28 @@
-/******************************************************************************
-* Copyright © 2005-2013 by Pike Aerospace Research Corporation
-* All Rights Reserved
-*
-*   This file is part of CARIBOU RTOS
-*
-*   CARIBOU RTOS is free software: you can redistribute it and/or modify
-*   it under the terms of the Beerware License Version 43.
-*
-* ----------------------------------------------------------------------------
-* "THE BEER-WARE LICENSE" (Revision 43):
-* <mike@pikeaero.com> wrote this file. As long as you retain this notice you
-* can do whatever you want with this stuff. If we meet some day, and you think
-* this stuff is worth it, you can buy me a beer in return ~ Mike Sharkey
-* ----------------------------------------------------------------------------
-******************************************************************************/
+/*
+   _________    ____  ________  ____  __  __   ____  __________  _____
+  / ____/   |  / __ \/  _/ __ )/ __ \/ / / /  / __ \/_  __/ __ \/ ___/
+ / /   / /| | / /_/ // // __  / / / / / / /  / /_/ / / / / / / /\__ \ 
+/ /___/ ___ |/ _, _// // /_/ / /_/ / /_/ /  / _, _/ / / / /_/ /___/ / 
+\____/_/  |_/_/ |_/___/_____/\____/\____/  /_/ |_| /_/  \____//____/  
+                                                                      
+
+Copyright © 2014-2015 by Mike Sharkey
+Copyleft © 2018 by Mike Sharkey 
+mike@pikeaero.com        
+
+This file is part of CARIBOU RTOS
+
+CARIBOU RTOS is free software: you can redistribute it and/or modify
+it under the terms of the Beerware License Version 43.
+
+----------------------------------------------------------------------------
+"THE BEER-WARE LICENSE" (Revision 43):
+<mike@pikeaero.com> wrote this file. As long as you retain this notice you
+can do whatever you want with this stuff. If we meet some day, and you think
+this stuff is worth it, you can buy me a beer in return ~ Mike Sharkey
+---------------------------------------------------------------------------- 
+
+*/
 #ifndef _CARIBOU_CPU_ARM_CORTEX_M0_H_
 #define _CARIBOU_CPU_ARM_CORTEX_M0_H_
 
