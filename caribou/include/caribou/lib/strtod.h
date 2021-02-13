117b2878b6f81534c9670b386d4fc1cf3dcb0ae2
8bitgeek <12674614+8bitgeek@users.noreply.github.com>
2021-02-12 23:46:54 -0500

    Create strtod.h

---
+++ caribou/include/caribou/lib/strtod.h
@@ -0,0 +1,14 @@
+#ifndef __CARIBOU_STRTOD_H__
+#define __CARIBOU_STRTOD_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern double strtod(const char *nptr, char **endptr);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
