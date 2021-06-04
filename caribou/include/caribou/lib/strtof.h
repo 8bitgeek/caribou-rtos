d855a0ef7c2034668e367a5f54881602bba92cec
8bitgeek <mike@8bitgeek.net>
2021-06-04 20:39:16 +0000

    add strtof

---
+++ caribou/include/caribou/lib/strtof.h
@@ -0,0 +1,14 @@
+#ifndef __CARIBOU_STRTOF_H__
+#define __CARIBOU_STRTOF_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern float strtof(const char *nptr, char **endptr);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
