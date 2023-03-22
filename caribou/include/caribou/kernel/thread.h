0712ef5b4a2b9369306bc2095bbe7de0ec089554
8bitgeek <mike@8bitgeek.net>
2023-03-21 22:20:08 -0400

    fast lock/unlock

--- caribou/include/caribou/kernel/thread.h
+++ caribou/include/caribou/kernel/thread.h
@@ -224,6 +224,8 @@ extern uint16_t				caribou_thread_state(caribou_thread_t* thread);
 
 #define 					caribou_thread_lock()			{if(caribou_state.current) ++caribou_state.current->lock;}
 #define 					caribou_thread_unlock()			{if(caribou_state.current) --caribou_state.current->lock;}
+#define 					caribou_thread_fast_lock()		(++caribou_state.current->lock)
+#define 					caribou_thread_fast_unlock()	(--caribou_state.current->lock)
 #define 					caribou_thread_locked(thread)	(thread->lock)
 
 extern void					caribou_thread_sleep_current(caribou_tick_t ticks);
